80d6d85a3c6ed6413d11ab6de5abceb6cabee974
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-18 12:40:46 +0000

    Test: Journeylog will initialize new instance of Journey class

--- spec/JourneyLog_spec.rb
+++ spec/JourneyLog_spec.rb
@@ -1,9 +1,15 @@
 require 'JourneyLog'
-
+require 'Journey'
 
 RSpec.describe JourneyLog do
 
+let(:journey) { journey = double{'Journey.new'} }
 
+  subject {described_class.new(journey)}
+
+  it 'knows about journey' do
+    expect(subject.journey).to eq(journey)
+  end
 
 
 end
